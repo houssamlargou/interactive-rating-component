f125cf8258facaff99eb396a9796a7567708b654
Largou <houssamlargou9@gmail.com>
2025-02-10 14:49:24 +0100

    add tailwind

--- pages/main.js
+++ pages/main.js
@@ -16,3 +16,19 @@ function highlight(number) {
   // console.log(( button: button ));
   button.classList.add("number--highlighted");
 }
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        Orange: "hsl(25, 97%, 53%)",
+        White: "hsl(0, 0%, 100%)",
+        light: "hsla(0, 0%, 33%, 0.244)",
+        LightGrey: "hsl(217, 12%, 63%)",
+        DarkBlue: "hsl(213, 19%, 18%)",
+        VeryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+};
